10f2c6b55e2de9230fe88314c0a797ef1a280bb5
王鑫 <iamwx@foxmail.com>
2026-02-11 10:18:06 +0800

    fix rename vite file

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { cloudflare } from '@cloudflare/vite-plugin'
+// import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   plugins: [
     vue(),           // Vue 插件
-    cloudflare()     // Cloudflare Vite 插件
+    // cloudflare()     // Cloudflare Vite 插件
   ],
   build: {
     outDir: 'dist'   // 与 wrangler.toml 中的 dist_dir 一致
